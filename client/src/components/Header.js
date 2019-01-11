f811048e77c37b300f7c9012e8c6780b2b0199df
Nestor Tamares <jrtamares@gmail.com>
2019-01-10 23:31:09 -0600

    Add OAuth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
   return(
     <div className = "ui secondary pointing menu">
-      <Link to = "/" className = "item">Stuh-Ream</Link>
+      <Link to = "/" className = "item">
+        Stuh-Ream
+      </Link>
       <div className = "right menu">
-        <Link to = "/" className = "item">All Streams</Link>
+        <Link to = "/" className = "item">
+          All Streams
+        </Link>
+        <GoogleAuth />
       </div>
     </div>
   );
